cdfd292dc2fb55cdfd58c2d1408d533b6bd78be5
hacker2009 <miha.otk@gmail.com>
2022-02-07 22:38:45 +0300

    14. Longest Common Prefix

---
+++ longest_Common_Prefix.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        len_strs = len(strs)
+        if len_strs == 1:
+            return strs[0]
+        strs.sort(key = lambda s: len(s))
+        first_word = strs[0]
+        repeats = []
+        for i in range(1, len_strs):
+            s = ''
+            n = 0
+            while n <= len(first_word) - 1:
+                if first_word[n] == strs[i][n]:
+                    s += first_word[n]
+                else:
+                    n = len(first_word)
+                n += 1
+            repeats.append(s)
+        repeats.sort(key = lambda s: len(s))
+
+        return repeats[0]
+
+s = Solution()
+print(s.longestCommonPrefix(['']))
