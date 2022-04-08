e09d8066fdc2bb21c2be05a48ac64a2ddbec7101
hacker2009z <miha.otk@gmail.com>
2022-04-08 19:03:17 +0300

    136. Single Number

---
+++ singlenum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def singleNumber(self, nums: list[int]) -> int:
+        dictionary = {}
+        for i in range(len(nums)):
+            if nums[i] in dictionary:
+                dictionary[nums[i]] += 1
+            else:
+                dictionary[nums[i]] = 1
+        for i in dictionary.keys():
+            if dictionary[i] == 1:
+                print(i)
